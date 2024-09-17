3880283c98a62b41d847d208db68ab98e4ab110e
abdullahabbasi143 <129058605+abdullahabbasi143@users.noreply.github.com>
2024-09-17 09:20:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,14 +15,90 @@ export default function App(){
         </section>
       </div>
       <section id="about" className="bg-black text-white">
-        <div className="container mx-auto  px-6 py-16">
-        <div className="text-center md:text-left">
-  <h1 className="text-4xl font-bold md:text-5xl mt-5 mb-4">About Me</h1>
-  <p className="text-sm  mt-3">
-    I am a Junior Web Developer with one year of experience in both front-end and back-end technologies. On the front-end, I have been focusing on learning and utilizing React.js, while on the back-end, I have experience working with Laravel. I am passionate about continuous learning and improving my skills.
-  </p>
-</div>
+          <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
+        <div className="flex flex-col md:flex-row items-center md:space-x-12">
+          <img
+            src={AboutImage}
+            alt=""
+            className="w-72 h-80 rounded object-cover mb-8 md:mb-0"
+          />
+          <div className="flex-1">
+            <p className="text-lg mb-8">
+              I am a passionate Junior full-stack developer with one year expereinece  focus on building
+              modern and responsive web applications. With a strong foundation
+              in both frontend and backend technologies, I strive to create
+              seamless and efficient user experiences.
+            </p>
+            <div className="space-y-4">
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  HTML & CSS
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-10/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Tailwind Css & Bootstrap
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-10/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  React JS
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-9/12"
+                  ></div>  
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Laravel-Blade &&  Laravel-RestApi
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-11/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                   Wordpress 
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-9/12"
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 flex justify-between text-center">
+                <div>
+                    <h3 className="text-2xl font-bold text-transparent bg-clip-text 
+                bg-gradient-to-r from-green-400 to-blue-500">
+                        1
+                    </h3>
+                    <p>Years Experience</p>
+                </div>
+            </div>
+          </div>
         </div>
+      </div>
       </section>
 
       
